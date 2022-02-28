a1f7f7f719b1d01d67fb2e128cf0afa78a478bc8
PoojaSolankar <poojasolankar09gmail.com>
2022-02-28 23:26:34 +0530

    Employee Attendance

--- Employee.java
+++ Employee.java
@@ -1,10 +1,35 @@
-package javaApp;
+package com.Bridgelabz;
+import java.lang.Math.*;
 
-public class Employee {
+public class Employee 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-      System.out.println("welcome");
+	public static void main(String[] args)
+	{
+      System.out.println("Welcome");
+ 
+      int max = 2;
+      int min = 0;
+      int range = max - min +0;
+      
+      for (int i=0; i<=10; i++);
+      {
+    	  int rand = (int)(Math.random() * range) + min;
+    	  System.out.println(rand);
+    	  
+    	      switch(rand) 
+    	        {
+    	  
+    	           case 0: System.out.println("Emploee is absent");
+    	                    break;
+    	  
+    	           case 1: System.out.println("Employee is present");
+    	                    break;
+    	  
+    	           default:  System.out.println("end of the case");
+    	                     break;
+    	       }
+      }
+   
 	}
-
 }
