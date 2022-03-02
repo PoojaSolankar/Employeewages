17b04e05f529144649a317e7762962822f9d125b
PoojaSolankar <poojasolankar09gmail.com>
2022-03-02 17:20:27 +0530

    Employee Wage

--- Employee.java
+++ Employee.java
@@ -1,43 +1,36 @@
 package com.Bridgelabz;
-import java.lang.Math.*;
+import java.lang.Math;
+import java.util.Random;
 
 public class Employee 
 {
-
+    static final int WAGE_PER_HOURS =20;
 	public static void main(String[] args)
 	{
-      System.out.println("Welcome");
- 
-      int max = 2;
-      int min = 0;
-      int range = max - min +0;
-      int fulldayhour=8;
-      int workingHour=0;
-      int employeewageperHour=20;
-      for (int i=0; i<=10; i++);
-      {
-    	  int rand = (int)(Math.random() * range) + min;
-    	  System.out.println(rand);
-    	  
-    	      switch(rand) 
-    	        {
-    	  
-    	           case 0: System.out.println("Emploee is absent");
-    	                    workingHour=0;
-                            System.out.println("wokingHour is" +workingHour);
-    	                    break;
-    	  
-    	           case 1: System.out.println("Employee is present");
-    	                    workingHour=8;
-    	                    employeewageperHour=20;		
-    	                    System.out.println("wokingHour is : " +workingHour);
-    	                    System.out.println("employeewageperHour is : " +employeewageperHour);
-    	                    break;
-    	  
-    	           default:  System.out.println("end of the case");
-    	                     break;
-    	       }
-      }
+		
+	  int workingHours;
+      int Employeewag;
+      System.out.println("Welcome to Employee Wages");
+        
+     int randomVal = (int)Math.floor(Math.random()*10) %2;
+      
+     System.out.println("Random value:"+randomVal);
    
-	}
+     if(randomVal ==1) 
+     {
+    	 
+           System.out.println("Employee is Present");
+           workingHours=8;
+           System.out.println("Working hours :"+workingHours);
+           Employeewag=workingHours*WAGE_PER_HOURS;
+           System.out.println("Employeewag is :"+Employeewag);
+     }
+     else {
+           System.out.println("Employee is Absent");
+           workingHours=0;
+           System.out.println("Working hours :"+workingHours);
+         }
+   }
 }
+
+
