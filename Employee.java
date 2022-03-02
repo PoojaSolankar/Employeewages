85ed26f42b9ff062606104ecc6eb1bbaa84d3606
PoojaSolankar <poojasolankar09gmail.com>
2022-03-02 17:35:38 +0530

    Employee Working As Part Time

--- Employee.java
+++ Employee.java
@@ -6,6 +6,7 @@ public class Employee
 
 	public static void main(String[] args)
 	{
+<<<<<<< Updated upstream
       System.out.println("Welcome");
  
       int max = 4;
@@ -45,4 +46,40 @@ public class Employee
       }
    
 	}
+=======
+		
+	  int workingHours;
+      int Employeewag;
+      System.out.println("Welcome to Employee Wages");
+        
+     int randomVal = (int)Math.floor(Math.random()*10) %3;
+      
+     System.out.println("Random value:"+randomVal);
+   
+     if(randomVal ==1) 
+     {
+    	 
+           System.out.println("Employee is Present");
+           workingHours=8;
+           System.out.println("Working hours :"+workingHours);
+           Employeewag=workingHours*WAGE_PER_HOURS;
+           System.out.println("Employeewag is :"+Employeewag);
+     }
+     else if(randomVal==2)
+     {
+    	 System.out.println("Employee Working as Part Time");
+    	 workingHours=4;
+         System.out.println("Working hours :"+workingHours);
+         Employeewag=workingHours*WAGE_PER_HOURS;
+         System.out.println("Employeewag is :"+Employeewag);
+     }
+     else {
+           System.out.println("Employee is Absent");
+           workingHours=0;
+           System.out.println("Working hours :"+workingHours);
+     
+     
+     }
+   }
+>>>>>>> Stashed changes
 }
