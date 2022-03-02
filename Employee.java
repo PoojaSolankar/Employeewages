a4d3588f0c585bf3826291a02583a665feabe846
PoojaSolankar <poojasolankar09gmail.com>
2022-03-02 19:01:32 +0530

    Employee Wage For Month

--- Employee.java
+++ Employee.java
@@ -6,6 +6,7 @@ public class Employee
 {
     static final int WAGE_PER_HOURS = 20;
     static final int EMP_MONTH_DAYS = 20;
+    static final int EMP_MONTH_HOUR = 100;
     
 	public static void main(String[] args)
 	{
@@ -14,8 +15,11 @@ public class Employee
       int Employeewag=0;
       int totalworkingdays=1;
       int totalEmpWage=0;
+      int totalworkinghour=0;
+      int TotalHour=0;
+      int totalday=0;
       System.out.println("Welcome to Employee Wages");
-      while(totalworkingdays<20){
+      while(totalworkingdays<20 && totalworkinghour<100){
       
      int randomVal = (int)Math.floor(Math.random()*10) %4;
       
@@ -42,12 +46,15 @@ public class Employee
         default: System.out.println("End Of The Switch Case");
      
      }
+           
+           TotalHour=TotalHour+workingHours;
+           System.out.println("Total Working Hour is :" +TotalHour);
            Employeewag= workingHours * WAGE_PER_HOURS;
            System.out.println("Employeewag is :"+Employeewag);
            totalEmpWage= totalEmpWage+Employeewag;
            System.out.println("Total Emp Wage :"+totalEmpWage);
            totalworkingdays++;
-     
+           totalworkinghour++;
       }
    }
 }
