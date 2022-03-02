b915aed02c2cdbb411f0e35812d295003111f753
PoojaSolankar <poojasolankar09gmail.com>
2022-03-02 14:43:58 +0530

    Welcome

--- Employee.java
+++ Employee.java
@@ -2,9 +2,18 @@ package javaApp;
 
 public class Employee {
 
+<<<<<<< Updated upstream
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("welcome");
 	}
 
+=======
+	public static void main(String[] args)
+	{
+      System.out.println("Welcome to Employee Wages");
+ 
+      
+   }
+>>>>>>> Stashed changes
 }
