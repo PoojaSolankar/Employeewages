07b6dd2125dfb0e14d83782bf19d22c08822d41b
PoojaSolankar <poojasolankar09gmail.com>
2022-03-02 16:06:53 +0530

    Employee Attendence

--- Employee.java
+++ Employee.java
@@ -1,35 +1,26 @@
 package com.Bridgelabz;
-import java.lang.Math.*;
+import java.lang.Math;
+import java.util.Random;
 
 public class Employee 
 {
 
 	public static void main(String[] args)
 	{
-      System.out.println("Welcome");
- 
-      int max = 2;
-      int min = 0;
-      int range = max - min +0;
+      System.out.println("Welcome to Employee Wages");
+        
+     int randomVal = (int)Math.floor(Math.random()*10) %2;
       
-      for (int i=0; i<=10; i++);
-      {
-    	  int rand = (int)(Math.random() * range) + min;
-    	  System.out.println(rand);
-    	  
-    	      switch(rand) 
-    	        {
-    	  
-    	           case 0: System.out.println("Emploee is absent");
-    	                    break;
-    	  
-    	           case 1: System.out.println("Employee is present");
-    	                    break;
-    	  
-    	           default:  System.out.println("end of the case");
-    	                     break;
-    	       }
-      }
+     System.out.println("Random value:"+randomVal);
    
-	}
+     if(randomVal ==1) 
+     {
+           System.out.println("Employee is Present");
+     }
+     else
+           System.out.println("Employee is Absent");
+	
+     }
 }
+
+
