434c4b625e1f631e218d98c339effdbbbfb173c8
PoojaSolankar <99306025+PoojaSolankar@users.noreply.github.com>
2022-03-02 17:41:30 +0530

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,54 +1,13 @@
 package com.Bridgelabz;
-import java.lang.Math.*;
-
+import java.lang.Math;
+import java.util.Random;
 public class Employee 
 {
 
-	public static void main(String[] args)
-	{
-<<<<<<< Updated upstream
-      System.out.println("Welcome");
- 
-      int max = 4;
-      int min = 0;
-      int range = max - min +0;
-      int fulldayhour=8;
-      int workingHour=0;
-      int employeewageperHour=20;
-      for (int i=0; i<4; i++);
-      {
-    	  int rand = (int)(Math.random() * range) + min;
-    	  System.out.println(rand);
-    	  
-    	      switch(rand) 
-    	        {
-    	  
-    	           case 0: System.out.println("Emploee is absent");
-    	                    workingHour=0;
-                            System.out.println("wokingHour is" +workingHour);
-    	                    break;
-    	  
-    	           case 1: System.out.println("Employee is present");
-    	                    workingHour=8;
-    	                    employeewageperHour=20;		
-    	                    System.out.println("wokingHour is : " +workingHour);
-    	                    System.out.println("employeewageperHour is : " +employeewageperHour);
-    	                    break;
-    	                    
-    	           case 2: System.out.println("Employee is working as part time");
-                            workingHour=8;
-                             System.out.println("wokingHour is : " +workingHour);
-                              break;
-                              
-    	           default:  System.out.println("end of the case");
-    	                     break;
-    	       }
-      }
-   
-	}
-=======
+    public static void main(String[] args)
+    {
 		
-	  int workingHours;
+      int workingHours;
       int Employeewag;
       System.out.println("Welcome to Employee Wages");
         
@@ -77,9 +36,6 @@ public class Employee
            System.out.println("Employee is Absent");
            workingHours=0;
            System.out.println("Working hours :"+workingHours);
-     
-     
-     }
+        }
    }
->>>>>>> Stashed changes
 }
